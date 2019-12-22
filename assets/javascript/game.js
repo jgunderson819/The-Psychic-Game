70f283a018f9c7410d213545ff944151c75a19f0
Jeff <jgunderson819@gmail.com>
2019-12-22 17:16:03 -0600

    getting close to working

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var choices = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
+
+
+
+
+var computer = Math.floor(Math.random() * 26);
+
+
+
+for (i=1; i<11; i++) 
+document.onkeyup = function(event) {
+var userguess = alert "What is your guess"
+var wins = 0
+var losses = 0
+var guesses = 0
+function myfunction 
+if  userguess == choices(computer) {
+
+document.write ("You Win!");
+wins++;
+
+else
+guesses++;
+document.write ("Try again");
+
+}
+
+document.write ("You lose!");
+losses++
+
+
+
+}
